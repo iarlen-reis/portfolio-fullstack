8a164aeb8e3da9c847dbf4e8692ef9645a4b55a2
Iarlen <iarlensr@hotmail.com>
2023-08-01 00:48:16 -0300

    refactor: refactoring Header component.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,16 @@
 import MenuButton from './MenuButton'
-import MenuSide from './MenuSide'
+import MenuASide from './MenuSide'
 
 const Header = () => {
   return (
-    <header className="relative flex w-full items-center border-b border-zinc-50/40">
-      <div className="flex w-full items-center justify-between p-3 px-4">
-        <span className="font-marker">Iarlen Reis</span>
+    <header className="relative flex w-full items-center">
+      <div className="flex w-full items-center justify-between px-4 py-5">
+        <span className="font-marker text-xl">Iarlen Reis</span>
         <div className="flex items-center">
           <MenuButton />
         </div>
       </div>
-      <MenuSide />
+      <MenuASide />
     </header>
   )
 }
