226ceb6b1fbba3ef53fe821c1383d8b21a19a887
Iarlen <iarlensr@hotmail.com>
2023-08-01 00:47:42 -0300

    feat: creating Footer component.

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <footer className="w-full border-t border-white/10">
+      <div className="mx-auto w-full max-w-[1280px] px-3 py-5">
+        <a
+          href="https://github.com/Iarlen-reis"
+          target="_blank"
+          rel="noreferrer"
+          className="flex justify-center font-poppins text-sm italic transition-colors hover:text-zinc-300 sm:justify-end md:text-base"
+        >
+          © 2023 - Iarlen Reis
+        </a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
