1798f2ea62a6b174370fefe03bf5fb83429b7ced
Iarlen <iarlensr@hotmail.com>
2023-08-05 05:24:31 -0300

    feat: creating api routes to create and list all projects.

---
+++ src/app/api/projects/route.ts
@@ -0,0 +1,45 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { prisma } from '@/utils/prisma'
+import z from 'zod'
+
+export async function GET() {
+  const projects = await prisma.project.findMany()
+
+  return NextResponse.json({ projects })
+}
+
+export async function POST(request: NextRequest) {
+  const bodySchema = z.object({
+    title: z.string(),
+    description: z.string(),
+    image: z.string(),
+    alt: z.string(),
+    deployLink: z.string(),
+    githubLink: z.string(),
+    projectDate: z.string(),
+  })
+
+  const {
+    title,
+    description,
+    image,
+    alt,
+    deployLink,
+    githubLink,
+    projectDate,
+  } = bodySchema.parse(await request.json())
+
+  const project = await prisma.project.create({
+    data: {
+      title,
+      description,
+      image,
+      alt,
+      deployLink,
+      githubLink,
+      projectDate,
+    },
+  })
+
+  return NextResponse.json(project, { status: 200 })
+}
