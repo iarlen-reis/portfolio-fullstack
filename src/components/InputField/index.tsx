8e50c7c8f4f6662e8f1708cb31c3ca85707b21b4
Iarlen <iarlensr@hotmail.com>
2023-08-09 05:48:56 -0300

    feat: adding styling for when inputs are disabled.

--- src/components/InputField/index.tsx
+++ src/components/InputField/index.tsx
@@ -43,7 +43,7 @@ const TextField = ({
         {...inputProps}
         {...rest}
         ref={ref}
-        className="w-full rounded bg-zinc-300 px-3 py-2 font-poppins text-base text-zinc-900 outline-none focus:outline-blue-600 lg:text-lg"
+        className="w-full rounded bg-zinc-300 px-3 py-2 font-poppins text-base text-zinc-900 outline-none focus:outline-blue-600 disabled:animate-pulse disabled:bg-zinc-400 disabled:text-zinc-500 lg:text-lg"
       />
       {error?.message && (
         <span className="text-xs text-red-500">{error.message}</span>
--- src/components/TextAreaField/index.tsx
+++ src/components/TextAreaField/index.tsx
@@ -33,7 +33,7 @@ export const TextAreaField = ({
         {label}
       </label>
       <textarea
-        className="w-full rounded border-none bg-zinc-300 px-3 py-2 font-poppins text-base text-zinc-900 outline-none focus:outline-blue-600 lg:text-lg"
+        className="w-full rounded border-none bg-zinc-300 px-3 py-2 font-poppins text-base text-zinc-900 outline-none focus:outline-blue-600 disabled:animate-pulse disabled:bg-zinc-400 disabled:text-zinc-500 lg:text-lg"
         ref={ref}
         {...inputProps}
         {...rest}
