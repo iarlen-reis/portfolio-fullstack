fdefc6a12a0fd3aba93862b4b900cc78cbed08fb
Iarlen <iarlensr@hotmail.com>
2023-08-09 01:48:40 -0300

    refactor: refactoring the structure of the PageNavegation component, adding a nav instead of a paragraph.

--- src/components/PageNavigation/index.tsx
+++ src/components/PageNavigation/index.tsx
@@ -6,20 +6,43 @@ interface IPageNavigationProps {
   to: string
   link: string
 }
+
+/*
+ <div>
+        <p className="font-roboto text-sm transition-colors lg:text-base">
+          <Link
+            href={link}
+            aria-label="Voltar para a lista de projetos"
+            className="hover:font-semibold hover:text-zinc-400"
+          >
+            {to}
+          </Link>{' '}
+          &gt; <span className="font-semibold capitalize">{from}</span>
+        </p>
+      </div>
+*/
+
 const PageNavigation = ({ from, to, link }: IPageNavigationProps) => {
   return (
-    <div>
-      <p className="font-roboto text-sm transition-colors lg:text-base">
-        <Link
-          href={link}
-          aria-label="Voltar para a lista de projetos"
-          className="hover:font-semibold hover:text-zinc-400"
-        >
-          {to}
-        </Link>{' '}
-        &gt; <span className="font-semibold capitalize">{from}</span>
-      </p>
-    </div>
+    <>
+      <nav>
+        <ul className="flex items-center gap-1">
+          <li>
+            <Link
+              href={link}
+              aria-label="Voltar para a lista de projetos"
+              className="hover:font-semibold hover:text-zinc-400"
+            >
+              {to}
+            </Link>
+          </li>
+          <li>&gt;</li>
+          <li>
+            <span className="font-semibold capitalize">{from}</span>
+          </li>
+        </ul>
+      </nav>
+    </>
   )
 }
 
