f2e2971f7e474bca831009dbe0f2f6f6491eb42c
Iarlen <iarlensr@hotmail.com>
2023-08-10 23:55:11 -0300

    feat: adding MenuMobile component in Header and adding logo.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,33 @@
+import LinkNavigation from '../LinkNavigation'
+import { HomeIcon, PenSquareIcon, Code2Icon } from 'lucide-react'
 import MenuButton from './MenuButton'
-import MenuMobile from './MenuMobile'
+import MenuMobile from '../MenuMobile'
+import Logo from '/public/logo-1.png'
+import Image from 'next/image'
 
 const Header = () => {
   return (
     <header className="relative flex w-full items-center">
-      <div className="m-auto flex w-full max-w-[1280px] items-center justify-between px-4 py-5">
-        <span className="font-marker text-xl">Iarlen Reis</span>
-        <div className="flex items-center">
+      <div className="m-auto flex w-full max-w-[1280px] items-center justify-between px-4 py-3">
+        <Image src={Logo} alt="logo" width={130} height={70} />
+        <nav className="hidden md:flex">
+          <ul className="flex flex-row items-center gap-6">
+            <li>
+              <LinkNavigation name="Página inicial" to="/" icon={HomeIcon} />
+            </li>
+            <li>
+              <LinkNavigation
+                name="Sobre mim"
+                to="/sobre"
+                icon={PenSquareIcon}
+              />
+            </li>
+            <li>
+              <LinkNavigation name="Projetos" to="/projetos" icon={Code2Icon} />
+            </li>
+          </ul>
+        </nav>
+        <div className="md:hidden">
           <MenuButton />
           <MenuMobile />
         </div>
