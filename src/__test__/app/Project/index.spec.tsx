d75f0fce994b7507c5327fe958a076bccf6ebfbe
Iarlen <iarlensr@hotmail.com>
2023-08-15 17:06:48 -0300

    tests: adding import 'intersection-observer' in the PageNavigation component tests and the Project page.

--- src/__test__/app/Project/index.spec.tsx
+++ src/__test__/app/Project/index.spec.tsx
@@ -1,5 +1,6 @@
 import { render } from '@testing-library/react'
 import Project from '@/app/projetos/[id]/page'
+import 'intersection-observer'
 
 import axios from 'axios'
 jest.mock('axios')
--- src/__test__/components/PageNavigation/index.spec.tsx
+++ src/__test__/components/PageNavigation/index.spec.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import '@testing-library/jest-dom'
 import PageNavigation from '@/components/PageNavigation'
+import '@testing-library/jest-dom'
+import 'intersection-observer'
 
 const renderPageNavigation = () => {
   return render(
